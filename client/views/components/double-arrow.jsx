9421625c9aa29f58cad4ccb73cbd1e81f7cdc574
Kelby <kg@N86507.local>
2018-09-27 16:18:30 -0400

    Update animated double arrow component

--- client/views/components/double-arrow.jsx
+++ client/views/components/double-arrow.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import iconArrowDown from './img/icon-arrow-down.svg';
-require('./double-arrow.scss');
+import styled from 'styled-components';
+
+const DoubleArrowStyled = styled.div`
+  transform: ${props => props.transform || 'none' };
+`;
+
+const Arrow = styled.img`
+  display: block;
+  margin: -28px auto;
+  position: relative;
+  bottom: 0;
+  width: 48px;
+  height: auto;
+  transform: translateY(-32px);
+  animation: arrowBlink 1750ms cubic-bezier(0.18, 0.89, 0.32, 1.28) infinite;
+
+  &:nth-child(2) {
+    animation-delay: 250ms;
+  }
+
+  @keyframes arrowBlink {
+    to {
+      opacity: 0;
+    }
+  }
+`;
 
 const DoubleArrow = () => (
-  <div className="double-arrow-component">
-    <img className="icon-arrow-down" src={ iconArrowDown } alt="arrow" />
-    <img className="icon-arrow-down" src={ iconArrowDown } alt="arrow" />
-  </div>
+  <DoubleArrowStyled>
+    <Arrow src={ iconArrowDown } alt="arrow" />
+    <Arrow src={ iconArrowDown } alt="arrow" />
+  </DoubleArrowStyled>
 );
 
 export default DoubleArrow;
